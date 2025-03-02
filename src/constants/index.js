5d0935097023a35e68cc7d696616a1db0330385e
Dawson Knudtson <knudtdaw0000@gmail.com>
2025-03-01 21:17:36 -0600

    about tweaks and trying to add link in companies component

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,17 @@
-export const ABOUT_TEXT = `Hello im Dawson, I am currently a software engineer and a entrepreneur. I enjoy learning about pretty much anything - sales, marketing, finance, programming, business, entrepreneurship, etc. I am currently working on a marketing agency called 1'st Marketing where we offer high-converting copywriting and sales funnels to both info and coaching businesses. In the future I am looking to create a software business and then enventually create an energy drink business, because most of the energy drinks I have tried always have 1 of 2 problems : 1. They make me crash or 2. The ingredients are fucking garbage.  `;
+export const ABOUT_TEXT = ` I am currently a software engineer and an entrepreneur. 
+I grew up in a small town in Wisconsin and got a associates degree in computer science. 
+
+Still deciding whether I want to pursue a bachelors or not. 
+
+As mentioned I am sort of new to the whole entrepreneurship thing. 
+
+I have been working on a marketing agency that offers high-converting copywriting and sales funnels to both info and coaching businesses. 
+
+In the future I am looking to create a software business and then enventually create an energy drink business, because most of the energy drinks I have tried always have 1 of two problems : 
+
+1. They always make me crash or 
+
+2. The ingredients are garbage.  `;
 
 
 export const CONTACT = {
