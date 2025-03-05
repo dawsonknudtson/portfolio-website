14faf6bb9f1b448f78780306e21972e1a1db29a7
Dawson Knudtson <knudtdaw0000@gmail.com>
2025-03-04 18:08:16 -0600

    skills-blog

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -30,9 +30,36 @@ const blogPosts = {
         title: "Skills I am learning and why?",
         date: "Febuary 28, 2025",
         content: `
-            --
-            
-            [Full blog post content would go here...]
+            <div class="prose prose-invert max-w-none">
+                <p class="text-2xl font-bold mb-6">Some of the valuable skills that I am currently learning include:</p>
+                
+                <ul class="list-disc pl-6 mb-8">
+                    <li class="text-xl mb-2">Programming</li>
+                    <li class="text-xl mb-2">Marketing</li>
+                    <li class="text-xl mb-2">Sales</li>
+                    <li class="text-xl mb-2">Soft skills - improving communication and organization of thoughts/notes/projects</li>
+                </ul>
+                
+                <p class="text-xl font-semibold mb-4">Why these skills?</p>
+                
+                <p class="mb-6">Now most people reading this could agree that these are valuable skills. However I don't think everyone should hop on the bandwagon of learning skills for 1. the sake of learning skills or 2. learning a skills because some guru tells you too.</p>
+                
+                <p class="mb-6">Personally I am only learning these skills because they interest me - and fortunately enough these skills pay money. I have been programming for about 2 years and still feel like I know nothing.</p>
+                
+                <p class="mb-6">Sometimes I take days-weeks-months off of programming and it frustrates me, because i enjoy it a lot. Not really the code but more so the thought process behind it - being able to design whatever I want and then able to create it real time for myself is very rewarding. Then the creativity - most of the time I can solve problems or create things any way I want without being told what to do which is always a plus.</p>
+                
+                <p class="text-xl font-semibold mb-4">Sales and Marketing</p>
+                
+                <p class="mb-6">These skills are basically the same it's just one is 1-1 and the other is 1-many. I'm learning this because I truly feel that with how everything being digitalized - if you can capture someone attention is 3 seconds or less you can sell them something. As for marketing i've mainly been learning copywriting just because I feel it's the most important part, along with sales funnels and customer/client journey- stuff like that. Both of these skills in my opinion require you to know 2 specific things in order to be decent at these. Those are understanding human psychology as well as understand your audience.</p>
+                
+                <p class="text-xl font-semibold mb-4">Soft Skills</p>
+                
+                <p class="mb-6">These are pretty self explanatory - Being able to not only communicate to someone but to communicate articulately and effectively is the backbone of every other skill. Then with organization it's an interesting one - lots of trail and error - seeing what works for me personally and what doesn't. I personally find that having a todo list is super helpful - if and only if you don't obsess over completing it. In the past i've tried to eliminate a todo list and just work and it just clouds up my mind with lots of nonsense.</p>
+                
+                <p class="text-xl font-semibold mb-4">Adaptability</p>
+                
+                <p class="mb-6">Another skill I should've added to the top is adaptability. Some people might consider this a trait - I would argue it's a skill you learn over time. To put it bluntly - these are some tough times. Everything being digital, AI taking everyone's jobs(sooner rather than later), and then changes to financial systems like the introduction to crypto, nft's(scam) and online money. Being able to navigate during these times can be tough - However i've learned to sort of "ride the wave" and be at the forefront of these changes and I certainly don't think I will regret it.</p>
+            </div>
         `
     },
     "ai-in-software-engineering": {
